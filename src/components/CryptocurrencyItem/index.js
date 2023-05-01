0b1b9c11455b9c53d5638ad4fee4abf84ed515f3
venkateshshyamala <venkat.mai938@gmail.com>
2023-05-01 05:46:44 +0000

    code

---
+++ src/components/CryptocurrencyItem/index.js
@@ -0,0 +1,31 @@
+// Write your JS code here
+import './index.css'
+
+const CryptocurrencyItem = props => {
+  const {cryptocurrencyDetails} = props
+  const {
+    currencyLogoUrl,
+    currencyName,
+    usdValue,
+    euroValue,
+  } = cryptocurrencyDetails
+
+  return (
+    <li className="cryptocurrency-item">
+      <div className="logo-and-title-container" data-testId="loader">
+        <img
+          src={currencyLogoUrl}
+          alt={currencyName}
+          className="currency-logo"
+        />
+        <p className="currency-name">{currencyName}</p>
+      </div>
+      <div className="usd-and-euro-values-container">
+        <p className="currency-value">{usdValue}</p>
+        <p className="currency-value">{euroValue}</p>
+      </div>
+    </li>
+  )
+}
+
+export default CryptocurrencyItem
